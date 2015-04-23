30373ef7cae4dd142d16178a90b21224a06ce3f6
jedcore-devs <jedcore@hotmail.com>
2015-04-23 12:58:14 +0800

    Fixed Navigation and Slider
    Fixed Navigation bar on top added reveal effect on viewport.

--- longreach/index.php
+++ longreach/index.php
@@ -6,120 +6,120 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Longreach</title>
     <link href="css/app.css" rel="stylesheet">
+  
   </head>
   <body>
 
-    <nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Brand</a>
-    </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><strong>LONGREACH</strong></a>
         </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+       
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#" data-toggle="modal" data-target="#menu"><span class="glyphicon glyphicon-align-justify"></span> Navigate</a></li>
           </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-    
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+
+
+        <!--<div class="container">
+          <div class="slider-logo pull-left">
+            <span class="tetx-logo"><strong>LONGREACH</strong></span>
+          </div>
+          <div class="menu-toggle pull-right">
+           <a href="#" class="btn btn-default" data-toggle="modal" data-target="#menu"><span class="glyphicon glyphicon-align-justify"></span></a>
+          </div>
+        </div>-->
+
+
 
     <section class="homeslider">
-      <div class="carousel fade-carousel slide" data-ride="carousel" data-interval="4000" id="bs-carousel">
-          <!-- Overlay -->
-          <div class="overlay"></div>
+     <div class="carousel fade-carousel slide" data-ride="carousel" data-interval="4000" id="bs-carousel">
+        <!-- Overlay -->
+        <div class="overlay"></div>
 
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <li data-target="#bs-carousel" data-slide-to="0" class="active"></li>
-            <li data-target="#bs-carousel" data-slide-to="1"></li>
-            <li data-target="#bs-carousel" data-slide-to="2"></li>
-          </ol>
-          
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner">
-            <div class="item slides active">
-              <div class="slide-1"></div>
-              <div class="hero">
-                <hgroup>
-                    <h1>We are creative</h1>        
-                    <h3>Get start your next awesome project</h3>
-                </hgroup>
-                <button class="btn btn-hero btn-lg" role="button">See all features</button>
-              </div>
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#bs-carousel" data-slide-to="0" class="active"></li>
+          <li data-target="#bs-carousel" data-slide-to="1"></li>
+          <li data-target="#bs-carousel" data-slide-to="2"></li>
+        </ol>
+        
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+
+
+          <div class="item slides active">
+            <div class="slide-1"></div>
+            <div class="hero">
+              <hgroup>
+                  <h1 data-sr="spin -30deg enter right, hustle 20px reset">One stop shop for your BPO </h1>        
+                  <h3 data-sr="enter left, hustle 20px">Get start your next awesome project</h3>
+              </hgroup>
+              <button class="btn btn-hero btn-lg" role="button">See all features</button>
             </div>
-            <div class="item slides">
-              <div class="slide-2"></div>
-              <div class="hero">        
-                <hgroup>
-                    <h1>We are smart</h1>        
-                    <h3>Get start your next awesome project</h3>
-                </hgroup>       
-                <button class="btn btn-hero btn-lg" role="button">See all features</button>
-              </div>
+          </div>
+          <div class="item slides">
+            <div class="slide-2"></div>
+            <div class="hero">        
+              <hgroup>
+                  <h1>We are smart</h1>        
+                  <h3>Get start your next awesome project</h3>
+              </hgroup>       
+              <button class="btn btn-hero btn-lg" role="button">See all features</button>
             </div>
-            <div class="item slides">
-              <div class="slide-3"></div>
-              <div class="hero">        
-                <hgroup>
-                    <h1>We are amazing</h1>        
-                    <h3>Get start your next awesome project</h3>
-                </hgroup>
-                <button class="btn btn-hero btn-lg" role="button">See all features</button>
-              </div>
+          </div>
+          <div class="item slides">
+            <div class="slide-3"></div>
+            <div class="hero">        
+              <hgroup>
+                  <h1>We are amazing</h1>        
+                  <h3>Get start your next awesome project</h3>
+              </hgroup>
+              <button class="btn btn-hero btn-lg" role="button">See all features</button>
             </div>
-          </div> 
-        </div>
+          </div>
+        </div> 
+      </div>
     </section>
+    <div data-sr="enter left, hustle 20px"> Foo </div>
+    <div data-sr="wait 2.5s, ease-in-out 100px"> Bar </div>
+    <div data-sr="move 16px scale up 20%, over 2s"> Baz </div>
+    <div data-sr="enter bottom, roll 45deg, over 2s"> Bun </div>
 
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
-
+ 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type='text/javascript' src='javascripts/scrollReveal.js'></script>  
+    <script>
+          window.scrollReveal = new scrollReveal();
+    </script>
     <script src="javascripts/themejs.js"></script>
     <script src="javascripts/bootstrap.min.js"></script>
+    
   </body>
 </html>
